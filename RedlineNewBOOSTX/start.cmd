c1d17949e33beb00571a4b80ad9ae8413ca68dca
kkn4discord <147192974+kkn4discord@users.noreply.github.com>
2023-11-10 19:59:22 +0100

    Add files via upload

---
+++ RedlineNewBOOSTX/start.cmd
@@ -0,0 +1,11 @@
+title BOOSTX By Redline TEAM .gg/RLL
+@echo off
+cls
+color 9
+echo "                    hey < 3                         "
+echo " For Support or any problem Join the discord.gg/RLL "
+echo "                Starting the bot.                   "
+timeout /t 3
+cls
+python main.py
+pause
